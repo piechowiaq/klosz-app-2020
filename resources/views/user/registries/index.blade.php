6a4d68c8ce7df6f4f1118015ad5a9c603f122970
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-07-14 20:14:36 +0200

    Ogarniety  Table

--- resources/views/user/registries/index.blade.php
+++ resources/views/user/registries/index.blade.php
@@ -19,36 +19,109 @@
             </div>
         </div>
         {{--        @livewire('search-employees', ['company'=>$company])--}}
-
-
         {{--        <search>   </search>--}}
-        @foreach ($companyRegistries as $registry)
 
-            <div class="md:flex border  text-center md:text-left mb-1">
-                <div class="m-2 p-2 py-2 md:w-8/12 ">
-                    <a href="{{route('user.registries.show', ['registry'=> $registry, 'company'=>$company->id])}}">{{ $registry->name}}</a>
-                </div>
 
-                @foreach ($registry->reports->where('company_id', $company->id)->sortByDesc('report_date') as $report)
 
-                    @if($loop->first)
-                    <div class="md:w-4/12 flex justify-around">
-                        <div class=" mt-2 py-2 ">
-                            @if(  Carbon\Carbon::now()->subDays(30) > $report->expiry_date)
-                                <svg class="inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/></svg>
-                            @endif Wygasa: {{ $report->expiry_date}}
-                        </div>
-                        <div class=" px-2 bg-transparent text-black-700 hover:text-indigo-700 text-indigo-500 rounded m-2 py-2 ">
-                            <a href="{{ route('user.reports.download', ['company'=> $company->id, 'report'=>$report]) }}" class="">Download</a>
-                        </div>
+        <table class="table-fixed">
+            <thead>
+                <tr>
+                    <th class="w-6/12 px-4 py-2">Rejestr</th>
+                    <th class="w-1/12 px-4 py-2">Ściągnij</th>
+                    <th class="w-1/12 px-4 py-2">Status</th>
+                    <th class="w-3/12 px-4 py-2">Data ważności</th>
+                    <th class="w-1/12 px-4 py-2">Szczegóły</th>
+                </tr>
+            </thead>
+            <tbody>
 
-                    </div>
-                    @endif
+            @foreach ($companyRegistries as $registry)
 
-                @endforeach
+                <tr>
+                    <td class=" px-4 py-2">
 
-            </div>
-        @endforeach
+                        <a href="{{route('user.registries.show', ['registry'=> $registry, 'company'=>$company->id])}}">{{ $registry->name}}</a>
+
+                    </td>
+
+                @forelse ($registry->reports->where('company_id', $company->id)->sortByDesc('report_date') as $report)
+
+
+
+
+
+                    <td class=" px-4 py-2">
+
+
+
+                        <a href="{{ route('user.reports.download', ['company'=> $company->id, 'report'=>$report]) }}" class="">Download</a>
+
+
+                    </td>
+
+
+
+                        @if($loop->first)
+
+                    <td class=" px-4 py-2">
+
+                        @if(  Carbon\Carbon::now()->subDays(30) > $report->expiry_date)
+                            <svg class="inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/></svg>
+                        @endif
+
+                    </td>
+
+                    <td class="px-4 py-2">
+
+                        Wygasa: {{ $report->expiry_date}}
+
+                    </td>
+                        @endif
+
+
+                    @empty
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                @endforelse
+                    <td class=" px-4 py-2">
+
+                         Szczegóły
+
+                    </td>
+
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+
+
+
+
+
+
+{{--            <div class="md:flex border  text-center md:text-left mb-1">--}}
+{{--                <div class="m-2 p-2 py-2 md:w-8/12 ">--}}
+{{--                   --}}
+{{--                </div>--}}
+
+{{--             --}}
+{{--                    <div class="md:w-4/12 flex justify-around">--}}
+{{--                        <div class=" mt-2 py-2 ">--}}
+{{--                           --}}
+{{--                        </div>--}}
+{{--                        <div class=" px-2 bg-transparent text-black-700 hover:text-indigo-700 text-indigo-500 rounded m-2 py-2 ">--}}
+{{--                          --}}
+{{--                        </div>--}}
+
+{{--                    </div>--}}
+{{--                  --}}
+
+{{--              --}}
+
+{{--            </div>--}}
+
     </div>
 
 @endsection
