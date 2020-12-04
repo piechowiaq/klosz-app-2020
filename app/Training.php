48c8b41f50e5fb5bcf3ebf851422e8550dbe1ae7
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-12-04 22:41:42 +0100

    feat/Kl-14 Refactored Training model

--- app/Training.php
+++ app/Training.php
@@ -1,21 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
+use DateTime;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
-
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Training extends Model
 {
 //    use Searchable;
 
-    private const ID_COLUMN               = 'id';
+    private const ID_COLUMN          = 'id';
+    private const NAME_COLUMN        = 'name';
+    private const DESCRIPTION_COLUMN = 'description';
+    private const VALID_FOR_COLUMN   = 'valid_for';
+    private const CREATED_AT_COLUMN  = 'created_at';
+    private const UPDATED_AT_COLUMN  = 'updated_at';
 
-
+    /** @var array|string[] */
     protected $guarded = [];
 
-    public function getId(): string
+    public static function getRegistryById(string $id): ?self
+    {
+        return self::find($id);
+    }
+
+    /**
+     * @param array|string[] $ids
+     *
+     * @return Collection|self[]
+     */
+    public static function getRegistriesById(array $ids): Collection
+    {
+        return self::whereIn(self::ID_COLUMN, $ids)->get();
+    }
+
+    public function getID(): string
     {
         return (string) $this->attributes[self::ID_COLUMN];
     }
@@ -25,46 +48,160 @@ class Training extends Model
         $this->attributes[self::ID_COLUMN] = $id;
     }
 
-
-    public static function search($query)
+    public function getName(): string
     {
-        return empty($query) ? static::query()
-            : static::where('name', 'like', '%'.$query.'%');
+        return (string) $this->attributes[self::NAME_COLUMN];
     }
 
-    public function path()
+    public function setName(string $name): void
     {
-        return "/admin/trainings/{$this->id}";
+        $this->attributes[self::NAME_COLUMN] = $name;
     }
 
-    public function userpath($companyId)
+    public function getDescription(): string
     {
-        return "/$companyId/trainings/{$this->id}";
+        return $this->attributes[self::DESCRIPTION_COLUMN];
     }
 
-    public function departments()
+    public function setDescription(string $description): void
+    {
+        $this->attributes[self::DESCRIPTION_COLUMN] = $description;
+    }
+
+    public function getValidFor(): int
+    {
+        return (int) $this->attributes[self::VALID_FOR_COLUMN];
+    }
+
+    public function setValidFor(int $validFor): void
+    {
+        $this->attributes[self::VALID_FOR_COLUMN] = $validFor;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return new DateTime($this->attributes[self::CREATED_AT_COLUMN]);
+    }
+
+    public function setCreatedAtDateTime(DateTime $dateTime): void
+    {
+        $this->attributes[self::CREATED_AT_COLUMN] = $dateTime;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return new DateTime($this->attributes[self::UPDATED_AT_COLUMN]);
+    }
+
+    public function setUpdatedAtDateTime(DateTime $dateTime): void
+    {
+        $this->attributes[self::UPDATED_AT_COLUMN] = $dateTime;
+    }
+
+    public function departments(): Relation
     {
         return $this->belongsToMany(Department::class);
     }
 
-    public function positions()
+    public function getDepartments(): Collection
+    {
+        return $this->departments()->get();
+    }
+
+    /**
+     * @param array|string[] $ids
+     */
+    public function setDepartments(array $ids): void
+    {
+        $this->departments()->sync($ids);
+    }
+
+    public function positions(): Relation
     {
         return $this->belongsToMany(Position::class);
     }
 
-    public function employees()
+    public function getPositions(): Collection
+    {
+        return $this->positions()->get();
+    }
+
+    /**
+     * @param array|string[] $ids
+     */
+    public function setPositions(array $ids): void
+    {
+        $this->positions()->sync($ids);
+    }
+
+    public function employees(): Relation
     {
         return $this->belongsToMany(Employee::class);
     }
 
-    public function certificates()
+    public function getEmployees(): Collection
+    {
+        return $this->employees()->get();
+    }
+
+    /**
+     * @param array|string[] $ids
+     */
+    public function setEmployees(array $ids): void
+    {
+        $this->employees()->sync($ids);
+    }
+
+    public function certificates(): Relation
     {
         return $this->hasMany(Certificate::class);
     }
 
-    public function companies()
+    public function getCertificates(): Collection
+    {
+        return $this->certificates()->get();
+    }
+
+    /**
+     * @param array|string[] $ids
+     */
+    public function setCertificates(array $ids): void
+    {
+        $this->certificates()->sync($ids);
+    }
+
+    public function companies(): Relation
     {
         return $this->belongsToMany(Company::class);
+    }
+
+    public function getCompanies(): Collection
+    {
+        return $this->companies()->get();
+    }
+
+    /**
+     * @param array|string[] $ids
+     */
+    public function setCompanies(array $ids): void
+    {
+        $this->companies()->sync($ids);
+    }
+
+    public function path(): string
+    {
+        return '/admin/trainings/' . $this->getId();
+    }
+
+    public function userPath(Company $company): string
+    {
+        return '/' . $company->getId() . '/trainings/' . $this->getId();
+    }
+
+    public static function search($query)
+    {
+        return empty($query) ? static::query()
+            : static::where('name', 'like', '%' . $query . '%');
     }
 
 //    public function scopeCertified($query, $training)
@@ -74,5 +211,4 @@ class Training extends Model
 //                ->where('training_id', $training->id);
 //        })->get();
 //    }
-
 }
