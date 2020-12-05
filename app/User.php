66961a8c6d9a6007fadc6754dc4c6274245f2a5d
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-12-05 22:25:17 +0100

    UserContorller & User model refactored as per phpstan

--- app/User.php
+++ app/User.php
@@ -1,23 +1,125 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use DateTime;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    private const ID_COLUMN                = 'id';
+    private const NAME_COLUMN              = 'name';
+    private const SURNAME_COLUMN           = 'surname';
+    private const EMAIL_COLUMN             = 'email';
+    private const EMAIL_VERIFIED_AT_COLUMN = 'email_verified_at';
+    private const PASSWORD_COLUMN          = 'password';
+    private const CREATED_AT_COLUMN        = 'created_at';
+    private const UPDATED_AT_COLUMN        = 'updated_at';
+
+    /** @var array|string[] */
     protected $guarded = [];
+
+    public static function getUserById(string $id): ?self
+    {
+        return self::find($id);
+    }
+
+    /**
+     * @param array|string[] $ids
+     *
+     * @return Collection|self[]
+     */
+    public static function getUsersById(array $ids): Collection
+    {
+        return self::whereIn(self::ID_COLUMN, $ids)->get();
+    }
+
+    public function getID(): string
+    {
+        return (string) $this->attributes[self::ID_COLUMN];
+    }
+
+    public function setId(string $id): void
+    {
+        $this->attributes[self::ID_COLUMN] = $id;
+    }
+
+    public function getName(): string
+    {
+        return (string) $this->attributes[self::NAME_COLUMN];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes[self::NAME_COLUMN] = $name;
+    }
+
+    public function getSurname(): string
+    {
+        return (string) $this->attributes[self::SURNAME_COLUMN];
+    }
+
+    public function setSurname(string $surname): void
+    {
+        $this->attributes[self::SURNAME_COLUMN] = $surname;
+    }
+
+    public function getEmail(): string
+    {
+        return (string) $this->attributes[self::EMAIL_COLUMN];
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes[self::EMAIL_COLUMN] = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return (string) $this->attributes[self::PASSWORD_COLUMN];
+    }
+
+    public function setPassword(int $email): void
+    {
+        $this->attributes[self::PASSWORD_COLUMN] = $email;
+    }
+
+    public function getEmailVerifiedAt(): DateTime
+    {
+        return new DateTime($this->attributes[self::EMAIL_VERIFIED_AT_COLUMN]);
+    }
+
+    public function setEmailVerifiedAt(DateTime $dateTime): void
+    {
+        $this->attributes[self::EMAIL_VERIFIED_AT_COLUMN] = $dateTime;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return new DateTime($this->attributes[self::CREATED_AT_COLUMN]);
+    }
+
+    public function setCreatedAtDateTime(DateTime $dateTime): void
+    {
+        $this->attributes[self::CREATED_AT_COLUMN] = $dateTime;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return new DateTime($this->attributes[self::UPDATED_AT_COLUMN]);
+    }
+
+    public function setUpdatedAtDateTime(DateTime $dateTime): void
+    {
+        $this->attributes[self::UPDATED_AT_COLUMN] = $dateTime;
+    }
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,7 +127,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
@@ -33,37 +136,62 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = ['email_verified_at' => 'datetime'];
+
     /**
      * The role associated with the user.
-     *
-     * @return BelongsToMany
      */
-    public function roles()
+    public function roles(): Relation
     {
         return $this->belongsToMany(Role::class)->withTimestamps();
     }
 
-    public function companies()
+    /**
+     * @return Collection|Company[]
+     */
+    public function getRoles(): Collection
     {
-        return $this->belongsToMany(Company::class)->withTimestamps();
+        return $this->roles()->get();
     }
 
-    public function path()
+    /**
+     * @param Collection|Role[] $roles
+     */
+    public function setRoles(Collection $roles): void
     {
-        return "/admin/users/{$this->id}";
+        $this->roles()->sync($roles);
     }
 
-    public function isSuperAdmin()
+    public function companies(): Relation
+    {
+        return $this->belongsToMany(Company::class);
+    }
+
+    public function getCompanies(): Collection
+    {
+        return $this->companies()->get();
+    }
+
+    /**
+     * @param Collection|Company[] $companies
+     */
+    public function setCompanies(Collection $companies): void
+    {
+        $this->companies()->sync($companies);
+    }
+
+    public function path(): string
+    {
+        return '/admin/users/' . $this->getID();
+    }
+
+    public function isSuperAdmin(): bool
     {
         return $this->roles()->where('name', 'SuperAdmin')->exists();
     }
 
-    public function getFullNameAttribute()
+    public function getFullNameAttribute(): string
     {
-        return $this->name.' '.$this->surname;
+        return $this->getName() . ' ' . $this->getSurname();
     }
-
 }
