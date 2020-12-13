d5ce5603fc7438f04af6e2656821c6d7d1851009
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-12-13 15:22:48 +0100

    feat/KL-14 Refactoring UpdateRegistryRequest as per phpstan

--- app/Http/Requests/UpdateRegistryRequest.php
+++ app/Http/Requests/UpdateRegistryRequest.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Registry;
+use Exception;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+
+use function assert;
 
 class UpdateRegistryRequest extends FormRequest
 {
@@ -24,8 +28,12 @@ class UpdateRegistryRequest extends FormRequest
      */
     public function rules(): array
     {
+        if (! assert($this->route('registry') instanceof Registry)) {
+            throw new Exception('Received registry is not the required object');
+        }
+
         return [
-            'name' => ['required','sometimes', Rule::unique('registries', 'name')->ignore($this->registry)],
+            'name' => ['required','sometimes', Rule::unique('registries', 'name')->ignore($this->route('registry')->getId())],
             'description' => 'required|sometimes',
             'valid_for' => 'required||sometimes',
         ];
