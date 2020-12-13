c4dd5b59e896ce1cbf49407ae31925f8325db532
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-12-13 15:28:25 +0100

    feat/KL-14 Refactoring UpdatePositionRequest as per phpstan

--- app/Http/Requests/UpdatePositionRequest.php
+++ app/Http/Requests/UpdatePositionRequest.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Position;
+use Exception;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+
+use function assert;
 
 class UpdatePositionRequest extends FormRequest
 {
@@ -21,11 +25,17 @@ class UpdatePositionRequest extends FormRequest
      * Get the validation rules that apply to the request.
      *
      * @return array|mixed[]
+     *
+     * @throws Exception
      */
     public function rules(): array
     {
+        if (! assert($this->route('position') instanceof Position)) {
+            throw new Exception('Received position is not the required object');
+        }
+
         return [
-            'name' => ['required','sometimes', Rule::unique('positions', 'name')->ignore($this->position)],
+            'name' => ['required','sometimes', Rule::unique('positions', 'name')->ignore($this->route('position')->getId())],
             'department_id' => 'exists:departments,id|required|sometimes',
         ];
     }
