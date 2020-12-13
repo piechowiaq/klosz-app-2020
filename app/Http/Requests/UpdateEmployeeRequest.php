195adf38d46d14a7fdff2e2827e261d7659aee88
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-12-13 16:47:51 +0100

    feat/KL-14 Refactoring UpdateEmployeeRequest

--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Employee;
+use Exception;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+
+use function assert;
 
 class UpdateEmployeeRequest extends FormRequest
 {
@@ -21,13 +25,19 @@ class UpdateEmployeeRequest extends FormRequest
      * Get the validation rules that apply to the request.
      *
      * @return array|mixed[]
+     *
+     * @throws Exception
      */
     public function rules(): array
     {
+        if (! assert($this->route('employee') instanceof Employee)) {
+            throw new Exception('Received employee is not the required object');
+        }
+
         return [
             'name' => 'sometimes|required',
             'surname' => 'sometimes|required',
-            'number' => ['required','sometimes', Rule::unique('employees', 'number')->ignore($this->employee)],
+            'number' => ['required','sometimes', Rule::unique('employees', 'number')->ignore($this->route('employee')->getId())],
             'company_id' => 'exists:companies,id|required|sometimes',
             'position_id' => 'exists:positions,id|required|sometimes',
         ];
