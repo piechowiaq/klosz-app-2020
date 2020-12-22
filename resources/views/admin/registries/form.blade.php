c4d6050339c069b3efe40e1a0be3e7fa9be41371
Bartosz Piechowiak <piechowiaq@gmail.com>
2020-12-22 19:58:18 +0100

    feat/KL-14 Refactoring views/admin/registries

--- resources/views/admin/registries/form.blade.php
+++ resources/views/admin/registries/form.blade.php
@@ -1,26 +1,21 @@
+@php
+    /**
+    * @var App\Registry $registry
+    */
+@endphp
+
 <div>
     <label for="name" class="block mt-2 py-2">Nazwa Szkolenia:</label>
-    <input type="text" name="name" value="{{old('name',  $registry->name) }}" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-500 focus:shadow-outline {{ $errors->has('name') ? 'is-invalid' : '' }}" required>
+    <input type="text" name="name" value="{{old('name',  isset($registry) ? $registry->getName() : '') }}" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-500 focus:shadow-outline {{ $errors->has('name') ? 'is-invalid' : '' }}" required>
 </div>
-
-{{--<div class="form-group">--}}
-{{--    <label for="active">Status pracownika:</label>--}}
-{{--    <select name="active" id="active" class="form-control  {{ $errors->has('active') ? 'is-invalid' : '' }}" required>--}}
-{{--        <option value="" disabled>Wybierz status pracownika</option>--}}
-{{--        <option value="1" {{$employee->active == 1 ? 'selected' : ''}}>Aktywny</option>--}}
-{{--        <option value="0" {{$employee->active == 0 ? 'selected' : ''}}>Nieaktywny</option>--}}
-{{--    </select>--}}
-{{--</div>--}}
-
-
 <div>
     <label for="description">Opis Rejestru:</label>
-    <textarea type="text" name="description" rows="3" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-500 focus:shadow-outline  {{ $errors->has('description') ? 'is-invalid' : '' }}">{{old('description') ?? $registry->description }}</textarea>
+    <textarea type="text" name="description" rows="3" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-500 focus:shadow-outline  {{ $errors->has('description') ? 'is-invalid' : '' }}">{{old('description', isset($registry) ? $registry->getDescription() : '') }}</textarea>
 </div>
 
 <div>
     <label for="valid_for">Ważne przez okres:</label>
-    <input type="number" name="valid_for" value="{{old('valid_for') ?? $registry->valid_for}}" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-500 focus:shadow-outline  {{ $errors->has('valid-for') ? 'is-invalid' : '' }}">
+    <input type="number" name="valid_for" value="{{old('valid_for', isset($registry) ? $registry->getValidFor() : '')}}" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-500 focus:shadow-outline  {{ $errors->has('valid-for') ? 'is-invalid' : '' }}">
 </div>
 
 
